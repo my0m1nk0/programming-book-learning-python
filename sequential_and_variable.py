a5c736dccfde931c8b160f777e3093c86b4ee53e
my0m1nk0 <myominko.msdev@gmail.com>
2024-10-23 09:16:15 +0630

    variable

---
+++ sequential_and_variable.py
@@ -0,0 +1,11 @@
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+boolean = True         # Boolean Value True and False only
+i = 5 + 4
+i = i  + 6
+print (i)
+print (counter)
+print (miles)
+print (name)
+print(boolean)
