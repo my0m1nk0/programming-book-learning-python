9f16581aeea8e24779e994c8114195869db6aabf
my0m1nk0 <myominko.msdev@gmail.com>
2024-10-24 08:49:04 +0630

    variable

---
+++ syntax.py
@@ -0,0 +1,22 @@
+k=5
+data= 'ground'
+if(data != 'floor'): #ture
+    k = 10
+    if(k == 11): #true
+        k = 15
+    else:
+        k = 11     
+else: #false
+    k = 20    
+print(k);  
+
+i = 5 + 4 
+i = i  + 6
+
+print (i)
+
+
+# data = 5 => Variable 
+# data == 5 => Expression
+# if(currentFloor > requestedFloor) => Statement
+#if(0 > 5) =>  Go to top 
---
+++ variable.py
@@ -0,0 +1,50 @@
+counter = 100 # An integer assignment
+miles = 1000.0 # A floating point
+name = "John" # A string
+boolean = True # Boolean Value True and False only
+
+# Integer , Float , String , Boolean
+
+name = "Naw Sel Sel Wah"
+age = 24 
+height = 5.5 
+is_student = True
+is_married = False
+gender = "Female"
+
+print("Name: ", name)
+print("Age: ", age)
+print("Height: ", height)
+print("Is Student: ", is_student)
+print("Is Married: ", is_married)
+print("Gender :", gender)
+print("**************************************")
+name = "Mr. Myo Min Ko"
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+print(name)
+# print("Myo Min Ko")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+# print("Myo Myint Aung")
+
